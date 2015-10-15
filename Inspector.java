eb7429c8e0a2407f3d38408b2687f1a334b34941
lnberry <lnberry@compute-linux01.cs.ucalgary.ca>
2015-10-15 12:37:06 -0600

    added loops as contingency for if there are not methods or fields

--- Inspector.java
+++ Inspector.java
@@ -10,16 +10,19 @@ public class Inspector
 	{
 		//name of declaring class
 		Class className=obj.getClass();
-		System.out.print ("Class name: ");
+		System.out.print ("\nClass name: ");
 		output(className);
+		System.out.println("");
 		
 		//name of immediate superclass
-		Class superClassName=className.getSuperclass();
+		Class superClassName=className.getSuperclass(); //Laura problem here: it is returning the class name not the super class name.
 		System.out.print ("Super Class name: ");
-		output(className);	
+		output(className);
+		System.out.println("");
 		
 		//method the class declares
 		Method[] methodArray=className.getDeclaredMethods();
+		System.out.println("Methods");
 		analizeMethods(methodArray);
 		
 		//constructors the class declares
@@ -28,51 +31,68 @@ public class Inspector
 			
 		//fileds the class declares
 		Field[] fields= className.getDeclaredFields();
+		System.out.println("Fields");
 		analizeFields(fields);
 
 	}
 	public void analizeFields(Field[] a)
 	{
-		for (int i=0; i<a.length; i++)
+		if(a.length==0)
 		{
-			System.out.print(i+1);
-			System.out.print(") Field Name: ");
-			String fieldName=a[i].toString();
-			System.out.println(fieldName);
-			//types
-			Class fieldType=a[i].getType();
-			System.out.print("	Field Type: ");
-			output(fieldType);
-			//modifiers	
-			int mods=a[i].getModifiers();
-			//Laura this also needs more work.
+			System.out.println("		none");
 		}
+		else
+		{
+			for (int i=0; i<a.length; i++)
+			{
+				System.out.print(i+1);
+				System.out.print(")\n 	Field Name: ");
+				String fieldName=a[i].toString();
+				System.out.println(fieldName);
+				//types
+				Class fieldType=a[i].getType();
+				System.out.print("		Field Type: ");
+				output(fieldType);
+				//modifiers	
+				int mods=a[i].getModifiers();
+				//Laura this also needs more work.
+			}
+		}
+		return;
 	}
 	public void analizeMethods(Method[] a)
-	{
-		for (int i=0; i<a.length; i++)
-		{	
-			System.out.print(i+1);
-			System.out.println(") Method name");
-			String methodName = a[i].toString();
-			System.out.println (methodName);
-			//exception thrown
-			Class[] exceptions = a[i].getExceptionTypes();
-			System.out.print("	exceptions thrown: ");
-			printArray(exceptions);
-			//parameter types
-			Class [] params = a[i].getParameterTypes();
-			System.out.print("	parameter types: ");
-			printArray(params);
-			//return types
-			Class whatIsReturned = a[i].getReturnType();
-			System.out.print("	Return type: ");
-			output(whatIsReturned);
-			//modifiers
-			int modifiers= a[i].getModifiers();
-			//Laura this needs more work
-			//it's currently giving an integer, the int needs to be decoded and analized
+	{		
+		if(a.length==0)
+		{
+			System.out.println("		none");
 		}
+		else
+		{
+			for (int i=0; i<a.length; i++)
+			{	
+				System.out.print(i+1);
+				System.out.print(")\n	 Method name: ");
+				String methodName = a[i].toString();
+				System.out.println (methodName);
+				//exception thrown
+				Class[] exceptions = a[i].getExceptionTypes();
+				System.out.print("		exceptions thrown: ");
+				printArray(exceptions);
+				//parameter types
+				Class [] params = a[i].getParameterTypes();
+				System.out.print("		parameter types: ");
+				printArray(params);
+				//return types
+				Class whatIsReturned = a[i].getReturnType();
+				System.out.print("		Return type: ");
+				output(whatIsReturned);
+				//modifiers
+				int modifiers= a[i].getModifiers();
+				//Laura this needs more work
+				//it's currently giving an integer, the int needs to be decoded and analized
+			}
+		}
+		return;
 	}
 	
 	public void printArray (Class[] a)
