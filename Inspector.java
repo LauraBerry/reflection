4f1fff627cefe8b1816f57b5d4be07ca315bf81b
lnberry <lnberry@compute-linux01.cs.ucalgary.ca>
2015-10-15 11:22:07 -0600

    added new code to analize methods, created a method for analizing methods (so meta)

--- Inspector.java
+++ Inspector.java
@@ -16,7 +16,7 @@ public class Inspector
 			classObject= Class.forName (args[0]);
 			//create an instance of the class
 			object = classObject.newInstance();
-			//inspect(object, true);
+			//inspect(object, true); //Laura
 		}
 		catch (InstantiationException e)
 		{
@@ -40,17 +40,62 @@ public class Inspector
 		//name of declaring class
 		Class className=obj.getClass();
 		//name of immediate superclass
+		System.out.print ("Class name: ");
+		output(className);
 		Class superClassName=className.getSuperclass();
 		//method the class declares
 		Method[] methodArray=className.getMethods();
-			//exception thrown
-			//parameter types
-			//return types//modifiers
+		analizeMethods(methodArray);
 		//constructors the class declares
 			//parameter types
 			//modifiers
 		//fileds the class declares
 			//types
 			//modifiers
+	}
+	
+	public void analizeMethods(Method[] a)
+	{
+		for (int i=0; i<a.length; i++)
+		{	
+			System.out.print(i);
+			System.out.println(") Method name");
+			String methodName = a[i].toString();
+			System.out.println (methodName);
+			//exception thrown
+			Class[] exceptions = a[i].getExceptionTypes();
+			System.out.print("exceptions thrown: ");
+			printArray(exceptions);
+			//parameter types
+			Class [] params = a[i].getParameterTypes();
+			System.out.print("parameter types: ");
+			printArray(params);
+			//return types
+			Class whatIsReturned = a[i].getReturnType();
+			System.out.print("Return type: ");
+			output(whatIsReturned);
+			//modifiers
+			int modifiers= a[i].getModifiers();
+			//Laura this needs more work
+		}
+	}
+	
+	public void printArray (Class[] a)
+	{
+		System.out.print("( ");
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.print(a[i]);
+			System.out.print(", ");
+		}
+		System.out.println(" )");
+		return;
+	}
+	
+	public void output(Class a)
+	{
+		String output= a.toString();
+		System.out.println(output);
+		return;
 	}
 }
